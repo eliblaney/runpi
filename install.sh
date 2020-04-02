d69ee18612cdabf2a8fb8988239a27e0168865f1
Eli Blaney <eliblaney@gmail.com>
2020-04-02 16:39:27 -0500

    Add safety and switch to wget

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "> Raspberry Pi QEMU emulator installer by Eli Blaney"
 OPTIND=1
 INSTALLDIR="~/.raspberrypi"
 
+# Process command line arguments
 while getopts "h?p:" opt; do
     case "$opt" in
     h|\?)
@@ -13,52 +14,72 @@ while getopts "h?p:" opt; do
 		echo "-p INSTALLDIR -- Change installation directory [default = ~/.raspberrypi]"
         exit 0
         ;;
-    p)  INSTALLDIR=$OPTARG
+    p)  INSTALLDIR="$OPTARG"
         ;;
     esac
 done
 
+# Shift arguments away, technically unnecssary here but good practice
 shift $((OPTIND-1))
-
 [ "${1:-}" = "--" ] && shift
 
 echo "> This script will install a Raspberry Pi emulator on your macOS computer."
 echo "> It will take up approximately 7 GB of space on your computer. Press Ctrl + C to abort."
 
-echo "> [1/8] Checking for Homebrew..."
-command -v brew >/dev/null 2>&1 || { echo >&2 "> Homebrew is not installed. You may install Homebrew at https://brew.sh.  Aborting."; exit 1; }
+echo "> [1/7] Installing dependencies..."
+$darwin = [[ "$(uname)" == "Darwin" ]];
+$brew = $(command -v brew >/dev/null 2>&1);
+$wget = $(command -v wget >/dev/null 2>&1);
+$qemu = $(command -v qemu >/dev/null 2>&1);
+$brewerror = "> Homebrew is not installed. You may install Homebrew at https://brew.sh.  Aborting.";
 
-echo "> [2/8] Installing cURL..."
-brew install curl
+$darwin && {
+	# macOS systems
+	$wget || {
+		echo "> Installing wget..."
+		$brew && brew install wget || {
+			echo >&2 "$brewerror"; exit 1;
+		}
+	}
+	$qemu || {
+		echo "> Installing QEMU..."
+		$brew && brew install qemu || {
+			echo >&2 "$brewerror"; exit 1;
+		}
+	}
+} || {
+	# Non macOS systems (likely Linux)
+	$wget || {
+		echo >&2 "Missing dependency: wget. Please use your package manager to install missing dependencies."; exit 1;
+	}
+	$qemu || {
+		echo >&2 "Missing dependency: QEMU. Please use your package manager to install missing dependencies."; exit 1;
+	}
+}
 
-echo "> [3/8] Creating installation directory at $INSTALLDIR..."
-mkdir $INSTALLDIR
+echo "> [2/7] Creating installation directory at $INSTALLDIR..."
+mkdir "$INSTALLDIR"
 
-echo "> [4/8] Downloading required scripts..."
+echo "> [3/7] Installing the Linux kernel..."
+wget -O "$INSTALLDIR/kernel-qemu-4.19.50-buster"  https://eliblaney.com/raspberrypi/kernel-qemu-4.19.50-buster
 
-curl https://github.com/eliblaney/runpi/uninstall.sh > $INSTALLDIR/uninstall.sh
-chmod +x $INSTALLDIR/uninstall.sh
+echo "> [4/7] Installing Raspbian \(this may take a while\)..."
+wget -O "$INSTALLDIR/raspbian-buster.qcow" https://eliblaney.com/raspberrypi/raspbian-buster.qcow
 
-curl https://github.com/eliblaney/runpi/runpi.sh > $INSTALLDIR/runpi.sh 
-chmod +x $INSTALLDIR/runpi.sh
+echo "> [5/7] Installing versatile-pb.dtb..."
+wget -O "$INSTALLDIR/versatile-pb.dtb" https://eliblaney.com/raspberrypi/versatile-pb.dtb
+echo "> [6/7] Downloading required scripts..."
 
+wget -O "$INSTALLDIR/uninstall.sh" https://github.com/eliblaney/runpi/uninstall.sh
+chmod +x "$INSTALLDIR/uninstall.sh"
+
+wget -O "$INSTALLDIR/runpi.sh"  https://github.com/eliblaney/runpi/runpi.sh
+chmod +x "$INSTALLDIR/runpi.sh"
+
+echo "> [7/7] Creating links..."
 echo "> Please enter your computer login password"
-sudo ln -sf $INSTALLDIR/runpi.sh /usr/local/bin/runpi
-
-echo "> [5/8] Installing QEMU..."
-brew install qemu
-
-echo "> [6/8] Installing the Linux kernel..."
-curl https://eliblaney.com/raspberrypi/kernel-qemu-4.19.50-buster > $INSTALLDIR/kernel-qemu-4.19.50-buster 
-
-echo "> [7/8] Installing Raspbian [this may take a while]..."
-curl https://eliblaney.com/raspberrypi/raspbian-buster.qcow > $INSTALLDIR/raspbian-buster.qcow 
-
-echo "> [8/8] Installing versatile-pb.dtb..."
-curl https://eliblaney.com/raspberrypi/versatile-pb.dtb > $INSTALLDIR/versatile-pb.dtb 
+sudo ln -sf "$INSTALLDIR/runpi.sh" /usr/local/bin/runpi
 
 echo "> Finished installing"
 echo "> Raspberry Pi installation can be found at $INSTALLDIR"
 echo "> Turn on your pi by typing \"runpi\""
-
-rm install.sh
